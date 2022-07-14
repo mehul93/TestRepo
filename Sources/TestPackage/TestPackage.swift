0f5ec36c3981d830bcb1c2a1994fd0ae4976fdb0
Mehul <mehul.mandalia@on.to>
2022-07-14 12:21:08 +0100

    add token

--- Sources/TestPackage/TestPackage.swift
+++ Sources/TestPackage/TestPackage.swift
@@ -1,6 +1,33 @@
+import Foundation
+
 public struct TestPackage {
     public private(set) var text = "Hello, World!"
-
+    
     public init() {
     }
+    
+    public func loadData() -> Token? {
+        guard let url = Bundle.main.url(forResource: "tokens", withExtension: "json")
+        else {
+            print("Json file not found")
+            return nil
+        }
+        
+        let data = try? Data(contentsOf: url)
+        let token = try? JSONDecoder().decode(Token.self, from: data!)
+        return token
+    }
 }
+
+// MARK: - TokenElement
+struct TokenElement: Codable {
+    let height, background: Background
+}
+
+// MARK: - Background
+struct Background: Codable {
+    let value, type: String
+}
+
+typealias Token = [TokenElement]
+
